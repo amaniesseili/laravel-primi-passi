51fe453fe4d5e511533b74d51fc30067f35bddfb
amani esseili <esseiliamani@hotmail.com>
2023-09-22 17:30:16 +0200

    bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,30 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/css/bootstrap.min.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="style.css">
+    <title>home page </title>
+</head>
+
 <body>
-<h1>hello world</h1>
+  <div class="container">
+    <h1>hello world</h1>
+
+  </div>
+
+    
 
 </body>
 
