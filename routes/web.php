2ce4d7642fd6b094e8eb5faa300140cc328d4040
amani esseili <esseiliamani@hotmail.com>
2023-09-22 17:17:54 +0200

    hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        "message" => "hello world"
+    ];
+    return view('home', $data);
 });
